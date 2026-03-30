8972b3364c80b842aed7712deaee6e327d3af4fc
Dhananjay-mangal <dhananjaymangalkrl@gmail.com>
2026-03-30 18:52:47 +0530

    Update API base URL to production endpoint

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
+    baseURL: "https://task-manager-d6w9.onrender.com",
     withCredentials: true,
 });
 
